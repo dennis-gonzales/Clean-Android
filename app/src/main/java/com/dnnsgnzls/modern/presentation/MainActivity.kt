6b205e892601be2dd6cac0c2823409cbb85c2262
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2023-07-17 20:52:46 +0800

    build(app+core+ui): add activity, fragments, VM, updated deps and target java version

---
+++ app/src/main/java/com/dnnsgnzls/modern/presentation/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.dnnsgnzls.modern.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dnnsgnzls.modern.R
+import com.dnnsgnzls.modern.presentation.note.ListFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, ListFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
